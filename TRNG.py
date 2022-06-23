f64b5a0cde8a251e67a0da4e992f7f0c88515a92
olghfwsptwerd <xq29ypr86yao@tutanota.com>
2022-06-23 19:02:01 +0200

    24-06-2022

---
+++ TRNG.py
@@ -0,0 +1,92 @@
+import imageio
+import numpy
+import math
+import cv2
+
+from itertools import chain
+from moviepy.editor import *
+ 
+
+
+numNeeded = 10000
+maxRange  = 256
+
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+numPossibles = 0
+schouldCont = True
+frame = 0
+bitsRange = math.ceil(math.log(maxRange, 2))
+tmp = 0
+count = 0
+sublist = []
+tempSublist = []
+outputList = []
+
+#==============================================================================
+# Przygotowanie klatek z filmu
+#==============================================================================
+
+def newImg():
+  return_value, image = camera.read()
+  img_name = "frame.png"
+  cv2.imwrite(img_name, image)
+
+#==============================================================================
+# Sczytywanie liczb z klatek
+#==============================================================================
+
+numNeeded *= bitsRange
+
+while(schouldCont):
+
+  newImg()
+
+  image = imageio.imread('frame.png')
+  imgHigth, imgWidth, imgChannel = image.shape
+
+  # Wczytywanie do listy pomocniczej wartosci z calej klatki
+  for i in range(0,imgHigth):
+    for j in range(0,imgWidth):
+      for k in range(0,imgChannel):
+        if(image[i][j][k] >= 2 and image[i][j][k] <= 253):
+          sublist.append(image[i][j][k] & 0b1)
+          numPossibles += 1
+  frame += 1 
+  if(numPossibles >= numNeeded):
+    schouldCont = False
+
+# Obliczanie wielkosci macierzy kwadratowej
+square = math.floor(math.sqrt(numNeeded))
+
+for i in range(0,square*square):
+  tempSublist.append(sublist[i])
+
+# Mieszanie macierzy
+tempSublist = numpy.array(tempSublist).reshape(square,square)
+transpose = tempSublist.T
+tempSublist = transpose.tolist()
+tempSublist = list(chain.from_iterable(tempSublist))
+
+# Jesli ilość liczb w macierzy kwadratowej jest niewystarczająca
+# dodawne sa liczby z ostatniej użytej klatki, od momentu uciecią jej
+# do macierzy kwadratowej
+if(square*square < numNeeded):
+  for i in range(0, numNeeded - square*square):
+    tempSublist.append(sublist[i+(square*square)])
+
+x = 0
+# Skladanie bitow w liczby z zakresu
+while(count < numNeeded/bitsRange):
+  for j in range(0,bitsRange):
+    valTmp = tempSublist[x]
+    tmp = tmp | (valTmp << j)
+    x += 1
+  outputList.append(tmp)
+  tmp = 0
+  count += 1
+
+output = open("output.txt", "w")
+for element in outputList:
+    output.write(str(element) + "\n")
+output.close()
+del(camera)
