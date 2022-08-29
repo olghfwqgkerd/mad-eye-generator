2de4d07b164abf61ca8932a733eb0a59ccdcb895
olghfwsptwerd <xq29ypr86yao@tutanota.com>
2022-08-29 18:08:29 +0200

    29-08-2022

--- TRNG.py
+++ TRNG.py
@@ -5,85 +5,93 @@ import numpy
 import math
 import cv2
  
-numNeeded = 10000
-maxRange  = 256
+class Beanser():
+    def __init__(self, numNeeded, maxRange):
+        self._numNeeded = numNeeded
+        self._maxRange = maxRange
+        self.camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        
 
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-numPossibles = 0
-schouldCont = True
-frame = 0
-bitsRange = math.ceil(math.log(maxRange, 2))
-tmp = 0
-count = 0
-sublist = []
-tempSublist = []
-outputList = []
+    numPossibles = 0
+    schouldCont = True
+    frame = 0
+    bitsRange = math.ceil(math.log(maxRange, 2))
+    tmp = 0
+    count = 0
+    sublist = []
+    tempSublist = []
+    outputList = []
 
-#==============================================================================
-# Przygotowanie klatek z filmu
-#==============================================================================
+    #==============================================================================
+    # Przygotowanie klatek z filmu
+    #==============================================================================
 
-def newImg():
-  return_value, image = camera.read()
-  img_name = "frame.png"
-  cv2.imwrite(img_name, image)
+    def newImg():
+        return_value, image = self.camera.read()
+        img_name = "frame.png"
+        cv2.imwrite(img_name, image)
 
-#==============================================================================
-# Sczytywanie liczb z klatek
-#==============================================================================
+    #==============================================================================
+    # Sczytywanie liczb z klatek
+    #==============================================================================
 
-numNeeded *= bitsRange
+    numNeeded *= bitsRange
 
-while(schouldCont):
+    while(schouldCont):
 
-  newImg()
+        newImg()
 
-  image = imageio.imread('frame.png')
-  imgHigth, imgWidth, imgChannel = image.shape
+        image = imageio.imread('frame.png')
+        imgHigth, imgWidth, imgChannel = image.shape
 
-  # Wczytywanie do listy pomocniczej wartosci z calej klatki
-  for i in range(0,imgHigth):
-    for j in range(0,imgWidth):
-      for k in range(0,imgChannel):
-        if(image[i][j][k] >= 2 and image[i][j][k] <= 253):
-          sublist.append(image[i][j][k] & 0b1)
-          numPossibles += 1
-  frame += 1 
-  if(numPossibles >= numNeeded):
-    schouldCont = False
+        # Wczytywanie do listy pomocniczej wartosci z calej klatki
+        for i in range(0,imgHigth):
+            for j in range(0,imgWidth):
+                for k in range(0,imgChannel):
+                    if(image[i][j][k] >= 2 and image[i][j][k] <= 253):
+                        sublist.append(image[i][j][k] & 0b1)
+                        numPossibles += 1
+        frame += 1 
+        if(numPossibles >= numNeeded):
+            schouldCont = False
 
-# Obliczanie wielkosci macierzy kwadratowej
-square = math.floor(math.sqrt(numNeeded))
+    # Obliczanie wielkosci macierzy kwadratowej
+    square = math.floor(math.sqrt(numNeeded))
 
-for i in range(0,square*square):
-  tempSublist.append(sublist[i])
+    for i in range(0,square*square):
+        tempSublist.append(sublist[i])
 
-# Mieszanie macierzy
-tempSublist = numpy.array(tempSublist).reshape(square,square)
-transpose = tempSublist.T
-tempSublist = transpose.tolist()
-tempSublist = list(chain.from_iterable(tempSublist))
+    # Mieszanie macierzy
+    tempSublist = numpy.array(tempSublist).reshape(square,square)
+    transpose = tempSublist.T
+    tempSublist = transpose.tolist()
+    tempSublist = list(chain.from_iterable(tempSublist))
 
-# Jesli ilość liczb w macierzy kwadratowej jest niewystarczająca
-# dodawne sa liczby z ostatniej użytej klatki, od momentu uciecią jej
-# do macierzy kwadratowej
-if(square*square < numNeeded):
-  for i in range(0, numNeeded - square*square):
-    tempSublist.append(sublist[i+(square*square)])
+    # Jesli ilość liczb w macierzy kwadratowej jest niewystarczająca
+    # dodawne sa liczby z ostatniej użytej klatki, od momentu uciecią jej
+    # do macierzy kwadratowej
+    if(square*square < numNeeded):
+        for i in range(0, numNeeded - square*square):
+            tempSublist.append(sublist[i+(square*square)])
 
-x = 0
-# Skladanie bitow w liczby z zakresu
-while(count < numNeeded/bitsRange):
-  for j in range(0,bitsRange):
-    valTmp = tempSublist[x]
-    tmp = tmp | (valTmp << j)
-    x += 1
-  outputList.append(tmp)
-  tmp = 0
-  count += 1
+    x = 0
+    # Skladanie bitow w liczby z zakresu
+    while(count < numNeeded/bitsRange):
+        for j in range(0,bitsRange):
+            valTmp = tempSublist[x]
+            tmp = tmp | (valTmp << j)
+            x += 1
+        outputList.append(tmp)
+        tmp = 0
+        count += 1
 
+    
+    del(camera)
+
+
+
+getBeans(10000, 256)
 output = open("output.txt", "w")
-for element in outputList:
-    output.write(str(element) + "\n")
-output.close()
-del(camera)
+    for element in outputList:
+            output.write(str(element) + "\n")
+    output.close()
